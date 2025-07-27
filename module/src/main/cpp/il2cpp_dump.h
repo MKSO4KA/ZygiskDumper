ff0be2e170d2084c33725e4a314e3bf08c444005
Mixailka MKSK <mk06ru@gmail.com>
2025-07-27 16:39:10 +0300

    update

--- module/src/main/cpp/il2cpp_dump.h
+++ module/src/main/cpp/il2cpp_dump.h
@@ -9,7 +9,5 @@ void il2cpp_api_init(void *handle);
 
 void il2cpp_dump(const char *outDir);
 
-// ИЗМЕНЕННАЯ СТРОКА: Объявляем нашу новую функцию-сканер
-bool find_and_dump_metadata(const char *outDir, int scan_id);
 
 #endif //ZYGISK_IL2CPPDUMPER_IL2CPP_DUMP_H
